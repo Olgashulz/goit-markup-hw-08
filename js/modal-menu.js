440cd1368d69e9da09607e9b73959a25de041808
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-04-12 15:12:18 +0300

    фото

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -1,16 +1,4 @@
-// (() => {
-//     const menuBtnRef = document.querySelector("[data-menu-button]");
-//     const mobileMenuRef = document.querySelector("[data-menu]");
-//     const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
-//     menuBtnRef.addEventListener("click", () => {
-//       mobileMenuRef.classList.toggle("is-open");
-//     });
-  
-//     mobileBtnClose.addEventListener("click", () => {
-//       mobileMenuRef.classList.toggle("is-open");
-//     });
-//   })();
+
 
   (() => {
     const menuBtnRef = document.querySelector ("[data-menu-button]");

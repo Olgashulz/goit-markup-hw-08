8d47a8b14a94cec71cf883b9d964f0d7503d4cee
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-04-14 13:18:25 +0300

    Добавила бекдроуп на меню

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -1,8 +1,24 @@
 
 
+  // (() => {
+  //   const menuBtnRef = document.querySelector ("[data-menu-button]");
+  //   const mobileMenuRef = document.querySelector ("[data-menu]");
+
+  //   menuBtnRef.addEventListener("click", () => {
+  //     const expanded =
+  //     menuBtnRef.getAttribute("aria-expanded") === true || false;
+
+  //     menuBtnRef.classList.toggle("is-open");
+  //     menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+  //     mobileMenuRef.classList.toggle("is-open");
+  //   })
+  // })();
+
   (() => {
     const menuBtnRef = document.querySelector ("[data-menu-button]");
     const mobileMenuRef = document.querySelector ("[data-menu]");
+    const mobileMenuBackdroupe = document.querySelector ("[data-backd]");
 
     menuBtnRef.addEventListener("click", () => {
       const expanded =
@@ -12,5 +28,6 @@
       menuBtnRef.setAttribute("aria-expanded", !expanded);
 
       mobileMenuRef.classList.toggle("is-open");
+      mobileMenuBackdroupe.classList.toggle("is-open");
     })
   })();
